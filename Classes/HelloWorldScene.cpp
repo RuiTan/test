70794602c27028a3667064e292e3ada296c820c3
RuiTan <1063932362@qq.com>
2022-05-06 23:30:21 +0800

    update

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -109,7 +109,7 @@ bool HelloWorld::init()
     else
     {
         // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+        sprite->setPosition(Vec2(visibleSize.width/3 + origin.x, visibleSize.height/3 + origin.y));
 
         // add the sprite as a child to this layer
         this->addChild(sprite, 0);
